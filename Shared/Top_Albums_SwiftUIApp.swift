8ea0ea2870aec4fc95defda8c45cef990d9520d6
Michael Redig <mredig@gmail.com>
2020-07-09 17:28:21 -0500

    initial commit

---
+++ Shared/Top_Albums_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Top_Albums_SwiftUIApp.swift
+//  Shared
+//
+//  Created by Michael Redig on 7/9/20.
+//
+
+import SwiftUI
+
+@main
+struct Top_Albums_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
